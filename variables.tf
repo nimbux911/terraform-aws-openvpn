ddee06564e734a1992495bcf1a392b2e137c6945
Diego Forziati <30295312+Ciejo@users.noreply.github.com>
2022-07-12 00:36:23 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,4 +4,5 @@ variable "subnet_ids" {}
 variable "ami_id" { default = "ami-02f3416038bdb17fb" } # Ubuntu 22.04 LTS
 variable "instance_type" { default = "t3a.micro" }
 variable "peered_networks" {}
-variable "docker_cidr" {}
+variable "docker_cidr" { description = "IP docker"}
+variable "compose_cidr" { description = "IP docker-compose"}
