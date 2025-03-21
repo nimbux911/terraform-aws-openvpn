baa3a949f5462f3d3d4c5c4af5ce98e846a27962
matias.marrapodi <matias.marrapodi.ext@plataforma10.com>
2025-03-21 16:08:10 -0300

    Fix ARN

--- backup.tf
+++ backup.tf
@@ -28,7 +28,7 @@ resource "aws_backup_selection" "this" {
   iam_role_arn = aws_iam_role.backup.arn
   plan_id      = aws_backup_plan.this.id
 
-  resources    = data.aws_instances.openvpn_instance.arns  
+  resources    = data.aws_instances.openvpn_instance.arn
 
   depends_on   = [aws_backup_plan.this, aws_iam_role.backup]
 }
